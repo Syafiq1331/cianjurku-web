1671619ff4da6d7cbe95dc30f2729bb998b14770
Syafiq1331 <bussinesyafiq@gmail.com>
2024-03-11 06:46:58 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::fallback(function () {
+    return view('pages.guest.404');
+});
+
+Route::get('/login', [\App\Http\Controllers\auth\AuthController::class, 'login'])->name('login');
