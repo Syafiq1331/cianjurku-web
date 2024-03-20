77bb2960a330e3af987b50f354d9f2b1fc31bd2a
Syafiq1331 <bussinesyafiq@gmail.com>
2024-03-20 07:59:15 +0700

    update kecamatan.blade.php, profileBupati.blade.php and profileWakilBupati.blade.php

---
+++ resources/views/pages/guest/pemerintahan/profileBupati.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.guest.app', ['title' => 'Dashboard'])
+
+@section('content')
+    <div class="container pt-3 pb-5 mt-2 mb-4">
+        <h1>Profil Bupati</h1>
+        <img src="{{ asset('landingPage/pemerintahan/profil-bupati.jpg') }}" alt="" class="w-100">
+    </div>
+@endsection
