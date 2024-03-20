69f0d5fe294c6ee998067370684abc8c5ee8bc74
Syafiq1331 <bussinesyafiq@gmail.com>
2024-03-20 07:59:24 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\guest\KontakController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,3 +18,17 @@ Route::prefix('profil')->group(function () {
     Route::get('/letak-geografis', [\App\Http\Controllers\guest\ProfilController::class, 'letakGeografis'])->name('letak-geografis');
     Route::get('/peta-administrasi', [\App\Http\Controllers\guest\ProfilController::class, 'petaAdministrasi'])->name('peta-administrasi');
 });
+
+Route::prefix('pemerintahan')->group(function () {
+    Route::get('/profil-bupati', [\App\Http\Controllers\guest\PemerintahanController::class, 'profilBupati'])->name('profil-bupati');
+    Route::get('/profil-wakil-bupati', [\App\Http\Controllers\guest\PemerintahanController::class, 'profilWakilBupati'])->name('profil-wakil-bupati');
+    Route::get('/sekretariat-daerah', [\App\Http\Controllers\guest\PemerintahanController::class, 'sekretariatDaerah'])->name('sekretariat-daerah');
+    Route::get('/dinas', [\App\Http\Controllers\guest\PemerintahanController::class, 'dinas'])->name('dinas');
+    Route::get('/badan', [\App\Http\Controllers\guest\PemerintahanController::class, 'badan'])->name('badan');
+    Route::get('/inspektor-daerah', [\App\Http\Controllers\guest\PemerintahanController::class, 'inspektorDaerah'])->name('inspektor-daerah');
+    Route::get('/sekretariat-dprd', [\App\Http\Controllers\guest\PemerintahanController::class, 'sekretariatDPRD'])->name('sekretariat-dprd');
+    Route::get('/rsud', [\App\Http\Controllers\guest\PemerintahanController::class, 'rsud'])->name('rsud');
+    Route::get('/kecamatan', [\App\Http\Controllers\guest\PemerintahanController::class, 'kecamatan'])->name('kecamatan');
+});
+
+Route::get('/kontak', [KontakController::class, 'index']);
